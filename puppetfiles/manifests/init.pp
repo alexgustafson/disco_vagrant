850c07e510c6a71d0c35294c4e5b9af1b326d076
Alex Gustafson <alexg.hsz@gmail.com>
2014-04-05 15:39:07 +0200

    vagrant file setup

---
+++ puppetfiles/manifests/init.pp
@@ -0,0 +1,59 @@
+class core {
+
+    exec { "apt-update":
+      command => "/usr/bin/sudo apt-get -y update"
+    }
+
+    package {
+      [ "vim", "git-core", "build-essential" ]:
+        ensure => ["installed"],
+        require => Exec['apt-update'],
+    }
+
+    exec { "debhelper":
+      command => "/usr/bin/sudo apt-get -y install debhelper"
+    }
+
+    exec { "tmux":
+      command => "/usr/bin/sudo apt-get -y intall tmux"
+    }
+
+    exec { "erlang":
+      command => "/usr/bin/sudo apt-get -y install erlang"
+    }
+
+    exec { "libcmph-dev":
+      command => "/usr/bin/sudo apt-get -y install libcmph-dev"
+    }
+}
+
+class python {
+
+    package {
+      [ "python3", "python-setuptools", "python-dev", "python-pip" ]:
+        ensure => ["installed"],
+        require => Exec['apt-update'],
+    }
+
+    exec {
+      "virtualenv":
+      command => "/usr/bin/sudo pip install virtualenv",
+
+    }
+
+}
+
+
+class networking {
+    package {
+      [ "snmp", "tkmib", "curl", "wget" ]:
+        ensure => ["installed"],
+        require => Exec['apt-update'],
+    }
+
+}
+
+include core
+include python
+include networking
+
