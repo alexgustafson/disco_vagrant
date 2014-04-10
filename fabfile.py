0da4b6f0892d3f006861936e358f69c69c7942bb
alexgustafson <alexg.hsz@gmail.com>
2014-04-10 21:05:17 +0200

    grab from prepackaged box

--- fabfile.py
+++ fabfile.py
@@ -1,93 +1,78 @@
-from fabric.api import env, local, run, cd, put
+from fabric.api import env, local, run, cd, put, roles
 
-def disco_master():
-    env.user = 'vagrant'
-    env.hosts = ["vagrant@192.168.50.4"]
-    env.password = "vagrant"
-    env.disco_repo = 'https://github.com/discoproject/disco.git'
-    env.disco_repo_branch = 'master'
-    env.disco_path = 'disco'
-    env.disco_absolute_path = '/home/vagrant/disco'
-    env.local_files = 'templates/discomaster'
-    env.master = True
+env.roledefs = {
+    'master': ['192.168.50.4'],
+    'nodes': ['192.168.50.10']
+}
 
+env.user = 'vagrant'
+env.password = "vagrant"
+DISCO_REPO = 'https://github.com/discoproject/disco.git'
+DISCO_REPO_BRANCH = 'master'
+DISCO_RELATIVE_PATH = 'disco'
+DISCO_ABSOLUTE_PATH = '/home/vagrant/disco'
 
-def disco_node_1():
-    env.user = 'vagrant'
-    env.hosts = ["vagrant@192.168.50.10"]
-    env.password = "vagrant"
-    env.disco_repo = 'https://github.com/discoproject/disco.git'
-    env.disco_repo_branch = 'master'
-    env.disco_path = 'disco'
-    env.disco_absolute_path = '/home/vagrant/disco'
-    env.local_files = 'templates/disconode1'
-    env.master = False
+MASTER_TEMPLATE_FILES = 'templates/discomaster'
+NODE01_TEMPLATE_FILES = 'templates/disconode1'
 
 
-def initialize():
-    #switch to master
-    print('** switching to discomaster **')
-    disco_master()
-
-    #do install step 1 on master
-    try:
-        print('** initial install step 1 on discomaster **')
-        install_step_01()
-    except Exception, e:
-        pass
-
-    #switch to disconode1
-    print('** switching to disconode1 **')
-    disco_node_1()
-
-    #do install step 1 on disconode1
-    try:
-        print('** initial install step 1 on disconode1 **')
-        install_step_01()
-    except Exception, e:
-        pass
-
-    #switch to master
-    print('** switching to discomaster **')
-    disco_master()
-    try:
-        print('** initial install step 2 on discomaster **')
-        install_step_02()
-    except Exception, e:
-        pass
-
-
+@roles('master', 'nodes')
 def install_step_01():
     #copy over custom hosts file so that we dont have to mess with any DNS
     print('copying hosts file to /etc/hosts')
-    put('{0}/hosts'.format(env.local_files), "/etc/hosts", use_sudo=True)
+    if env.host_string in env.roledefs['master']:
+        put('{0}/hosts'.format(MASTER_TEMPLATE_FILES), "/etc/hosts", use_sudo=True)
+    elif env.host_string in env.roledefs['nodes']:
+        put('{0}/hosts'.format(NODE01_TEMPLATE_FILES), "/etc/hosts", use_sudo=True)
 
     #clone the disco repository
     print('clone disco repo from github')
-    run('git clone {0} {1}'.format(env.disco_repo, env.disco_path))
+    try:
+        run('git clone {0} {1}'.format(DISCO_REPO, DISCO_RELATIVE_PATH))
+    except:
+        pass
 
     #make disco
     print('make disco')
-    with cd(env.disco_path):
-        run('make')
+    with cd(DISCO_RELATIVE_PATH):
+        try:
+            run('make')
+        except:
+            pass
+
+    if env.host_string in env.roledefs['master']:
+        with cd(DISCO_RELATIVE_PATH):
+            try:
+                run('make install')
+            except:
+                pass
+    else:
+        with cd(DISCO_RELATIVE_PATH):
+            try:
+                run('make install-node')
+            except:
+                pass
 
     #add disco command line utility to system path
     print('add disco command line utility to system path')
-    run('ln -s {0} /usr/local/bin'.format(env.disco_absolute_path))
+    try:
+        run('ln -s {0} /usr/local/bin'.format(DISCO_ABSOLUTE_PATH))
+    except:
+        pass
 
-
+@roles('master', 'nodes')
 def install_step_02():
-    if env.master:
+    if env.host_string in env.roledefs['master']:
         run("ssh-keygen -N '' -f ~/.ssh/id_dsa")
         run("ssh-copy-id disconode1")
-        run("scp ~/.erlang.cookie disconode1:")
+        run("scp ~/.erlang.cookie disconode1y")
 
-
+@roles('master')
 def start():
-    with cd(env.disco_path):
+    with cd(DISCO_RELATIVE_PATH):
         run('bin/disco nodaemon') # TODO : once setup if finished change this to make disco a deamon
 
-
+@roles('master')
 def stop():
-    with cd(env.disco_path):
+    with cd(DISCO_RELATIVE_PATH):
         run('bin/disco stop')
